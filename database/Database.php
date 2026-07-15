715c502a6542cd1f1e7f3243615899dba07b95f4
RenanAmor <renan@thenolimits.group>
2026-07-15 02:23:17 -0300

    Corrige conexão PDO

--- database/Database.php
+++ database/Database.php
@@ -3,6 +3,7 @@
 namespace Database;
 
 use PDO;
+use PDOException;
 
 class Database
 {
@@ -10,10 +11,14 @@ class Database
     {
         $env = parse_ini_file(__DIR__ . '/../.env');
 
+        if (!$env) {
+            throw new PDOException('.env não encontrado.');
+        }
+
         return new PDO(
-            "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8mb4",
-            $env['DB_USER'],
-            $env['DB_PASS'],
+            "mysql:host={$env['DB_HOST']};dbname={$env['DB_DATABASE']};charset=utf8mb4",
+            $env['DB_USERNAME'],
+            $env['DB_PASSWORD'],
             [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
