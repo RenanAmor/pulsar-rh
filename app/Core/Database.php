a22726df7ff0b454d77d1f8dfbcd7de85f8f8f45
RenanAmor <renan@thenolimits.group>
2026-07-15 02:11:01 -0300

    Corrige leitura das variáveis do banco

---
+++ app/Core/Database.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database;
+
+use PDO;
+use PDOException;
+
+class Database
+{
+    public static function connect(): PDO
+    {
+        $env = parse_ini_file(__DIR__ . '/../.env');
+
+        if (!$env) {
+            throw new PDOException('Arquivo .env não encontrado.');
+        }
+
+        return new PDO(
+            "mysql:host={$env['DB_HOST']};dbname={$env['DB_DATABASE']};charset=utf8mb4",
+            $env['DB_USERNAME'],
+            $env['DB_PASSWORD'],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );
+    }
+}
